b9ee01389dda8b703a61a741ef2c614bd69fe5b5
cauejlk <cauejoaquimlk@gmail.com>
2025-07-29 12:15:08 -0300

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><img src="" alt=""></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre-nos</a></li>
+        <li><a href="">Integrantes</a></li>
+        <li><a href="">Contatos</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
